a8086434b9aeac29e2bbc4029904b78bb3aef38e
cindyeme <emereninicynthia@gmail.com>
2023-10-03 16:18:00 +0100

    added pages, route parameters, NuxtLink, Layouts, using tailwindcss module

---
+++ pages/about.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h2 class="h2">About</h2>
+    <p class="p">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+      aspernatur ex voluptate illo voluptatum nisi quas ducimus laboriosam
+      sapiente praesentium incidunt repellendus ipsum, hic laborum tenetur
+      tempora facere quam quis!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit facilis,
+      quia odio ab velit voluptates hic et quae esse sed.
+    </p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+.p {
+  margin: 20px 0;
+}
+</style>
